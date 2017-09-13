b0c89d3db54aa712f1031f1cb860df6f50c1f094
sarahcodez <sarahgrahamx@gmail.com>
2017-09-12 23:00:42 -0600

    Added base Particle url to defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -1,7 +1,8 @@
 const defaults = {
   PHOTON_1: 'deviceId1',
   PHOTON_2: 'deviceId2',
-  PARTICLE_ACCESS_TOKEN: 'accessToken'
+  PARTICLE_ACCESS_TOKEN: 'accessToken',
+  PARTICLE_BASE_URL: 'https://api.particle.io'
 };
 
 module.exports = defaults;
